bd3939fd629aeeb4db6cca7d3e7d241e6dba405b
PRATIK N DAS <pratikndas@gmail.com>
2025-10-18 01:21:16 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  theme: {
-    extend: {
-      colors: { brand: { DEFAULT: "#0ea5e9", dark: "#0369a1" } }
-    }
-  },
-  plugins: []
+  theme: { extend: { colors: { brand: { DEFAULT: "#0ea5e9", dark: "#0369a1" } } } },
+  plugins: [],
 };
