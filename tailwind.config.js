6cd7898c84381606b2ea4aa4209a617a82c4227a
PRATIK N DAS <pratikndas@gmail.com>
2025-10-18 01:09:40 +0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: { brand: { DEFAULT: "#0ea5e9", dark: "#0369a1" } }
+    }
+  },
+  plugins: []
+};
